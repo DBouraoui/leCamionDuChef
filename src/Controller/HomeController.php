c6286db980ed676ef98a82279cd359dd9e595d24
DylanBro <dylan.bouraoui83@gmail.com>
2025-03-10 15:14:33 +0100

    feat(findUs): ajout d'affichage des horraire et emplacement.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\DishesRepository;
 use App\Repository\PicturesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(PicturesRepository $picturesRepository): Response
+    public function index(PicturesRepository $picturesRepository, DishesRepository $dishesRepository): Response
     {
         $pictures = $picturesRepository->findAll();
 
+        $dishes = $dishesRepository->findBy([],null,3);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             "pictures"=> $pictures,
+            'dishes' => $dishes
         ]);
     }
 }
